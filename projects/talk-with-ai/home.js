56813a0542cb7d6bbfeb9efb836e94da400e0e63
Shivam Gupta <shivamgupta96900@gmail.com>
2021-12-29 17:42:35 +0530

    website functionality enhanced

--- projects/talk-with-ai/home.js
+++ projects/talk-with-ai/home.js
@@ -1,21 +1,34 @@
+const link_arr = [];
+
 function add_msg() {
     var user_msg = document.getElementById('input-msg').value;
     if (user_msg == "") {
         alert("Please enter some message");
     }
     else {
-        make_msg_container(user_msg, 'user-msg-box');
+        make_msg_container(user_msg, 'user-msg-box', false);
         call_api(user_msg);
     }
 }
 
-function make_msg_container(msg_txt, class_name) {
+function make_msg_container(msg_txt, class_name, isLink) {
     var div_tag = document.createElement("div");
     div_tag.classList.add(class_name);
-    var p_tag = document.createElement("p");
-    var p_text = document.createTextNode(msg_txt);
-    p_tag.appendChild(p_text);
-    div_tag.appendChild(p_tag);
+
+    if (isLink) {
+        var a_tag = document.createElement('a'); //creatign a tag
+        var a_text = document.createTextNode(link_arr[0]); //creating text node
+        a_tag.appendChild(a_text); //adding text in the a tag
+        a_tag.href = link_arr[1];
+        div_tag.appendChild(a_tag); //adding a tag in the div tag
+    }
+    else {
+        var p_tag = document.createElement("p");
+        var p_text = document.createTextNode(msg_txt);
+        p_tag.appendChild(p_text);
+        div_tag.appendChild(p_tag);
+    }
+
     var msg_box = document.getElementById('msg_box');
     msg_box.classList.add('msg-box-class');
     msg_box.appendChild(div_tag);
@@ -34,8 +47,27 @@ function call_api(msg) {
         })
         .then((data) => {
             var ai_msg = data['cnt'];
-            make_msg_container(ai_msg, 'ai-msg-box');
+
+            if (ai_msg.substr(0, 2) == "<a") {
+                var link;
+                var link_text;
+                let idx1 = ai_msg.search("http");
+                let idx2 = ai_msg.search(">");
+                let idx3 = ai_msg.search("</a>");
+
+                link = ai_msg.substring(idx1, (idx2 - 1));
+                link_text = ai_msg.substring((idx2 + 1), idx3);
+                
+                link_arr[0] = link_text;
+                link_arr[1] = link;
+                make_msg_container(ai_msg, 'ai-msg-box', true);
+            }
+            else {
+                make_msg_container(ai_msg, 'ai-msg-box', false);
+            }
+
             console.log(data['cnt']);
+            console.log(link_arr);
         })
         .catch(err => {
             console.error(err);
